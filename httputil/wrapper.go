0999bf131f4707ec4426d4d89d003c47fc2d6981
Marc Bir <marc@rapidninja.com>
2021-10-18 15:56:04 -0700

    add wrapper for http.ResponseWriter to log response code

---
+++ httputil/wrapper.go
@@ -0,0 +1,39 @@
+package httputil
+
+import (
+	"net/http"
+)
+
+type ResponseWrapper struct {
+	http.ResponseWriter
+	status int
+	bytes  int
+}
+
+// NewWrapResponse wraps
+func NewWrapResponse(w http.ResponseWriter) *ResponseWrapper {
+	return &ResponseWrapper{w, 0, 0}
+}
+
+func (rw *ResponseWrapper) WriteHeader(code int) {
+	if rw.status == 0 {
+		rw.status = code
+		rw.ResponseWriter.WriteHeader(code)
+	}
+}
+
+func (rw *ResponseWrapper) Write(buf []byte) (int, error) {
+	rw.WriteHeader(http.StatusOK)
+	n, err := rw.ResponseWriter.Write(buf)
+	rw.bytes += n
+
+	return n, err
+}
+
+func (rw *ResponseWrapper) Status() int {
+	return rw.status
+}
+
+func (rw *ResponseWrapper) Bytes() int {
+	return rw.bytes
+}
