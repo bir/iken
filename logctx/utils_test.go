7aa34d6a75cb71256dc2ded3c2a8e74a1d5843a4
Marc Bir <marc@rapidninja.com>
2023-06-15 11:34:28 -0700

    propagate context values without cancel in logctx (#21)

--- logctx/utils_test.go
+++ logctx/utils_test.go
@@ -2,17 +2,23 @@ package logctx_test
 
 import (
 	"bytes"
+	"context"
 	"testing"
 
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/bir/iken/logctx"
 )
 
 func TestLogContext(t *testing.T) {
-	original := bytes.NewBuffer(nil)
-	ctx := logctx.NewSubLoggerContext(zerolog.New(original))
+	logBuffer := bytes.NewBuffer(nil)
+
+	id := "42"
+	ctx := logctx.SetID(context.Background(), id)
+	ctx, cancel := context.WithCancel(logctx.NewSubLoggerContext(ctx, zerolog.New(logBuffer)))
+
 	ctx2 := logctx.NewContextFrom(ctx)
 	logctx.AddToContext(ctx, "key", 1)
 	logctx.AddMapToContext(ctx, map[string]interface{}{"test": "value", "test2": "value2"})
@@ -23,7 +29,28 @@ func TestLogContext(t *testing.T) {
 
 	zerolog.Ctx(ctx2).Log().Msg("ctx2")
 
-	assert.Equal(t, original.String(), `{"key":1,"test":"value","test2":"value2","message":"ctx"}
+	assert.Equal(t, logBuffer.String(), `{"key":1,"test":"value","test2":"value2","message":"ctx"}
 {"key":"value","message":"ctx2"}
 `)
+	// ensure NewContext does not propagate cancel
+	cancel()
+	assert.Error(t, ctx.Err())
+	assert.NoError(t, ctx2.Err())
+
+	// ensure Context variables are shared
+	assert.Equal(t, id, logctx.GetID(ctx))
+	assert.Equal(t, id, logctx.GetID(ctx2))
+}
+
+func TestWithoutCancel(t *testing.T) {
+	assert.Panics(t, func() { logctx.WithoutCancel(nil) })
+	ctx := logctx.WithoutCancel(context.Background())
+
+	deadline, ok := ctx.Deadline()
+	assert.False(t, ok)
+	assert.Zero(t, deadline)
+
+	s, ok := ctx.(interface{ String() string })
+	require.True(t, ok)
+	assert.Equal(t, "context.Background.WithoutCancel", s.String())
 }
