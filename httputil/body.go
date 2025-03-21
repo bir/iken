1e20a5894c0ea2ccea3d1f80186a3a8651f6c3cf
Leon Clark <leon.clark@gmail.com>
2025-03-21 11:12:32 -0700

    GetJSONBody handle 'null': json.Unmarshal doesn't call custom unmarshalers if the body is 'null'

--- httputil/body.go
+++ httputil/body.go
@@ -4,18 +4,31 @@ import (
 	"encoding/json"
 	"errors"
 	"io"
+	"slices"
 
 	"github.com/bir/iken/validation"
 )
 
 var ErrMissingBody = validation.Error{Message: "missing body"}
 
+var nullBody = []byte("null")
+
 func GetJSONBody(r io.Reader, body any) error {
 	if r == nil {
 		return ErrMissingBody
 	}
 
-	err := json.NewDecoder(r).Decode(&body)
+	data, err := io.ReadAll(r)
+	if err != nil {
+		return validation.Error{Source: err}
+	}
+
+	// json.Unmarshal doesn't call custom unmarshalers if the body is null
+	if len(data) == 0 || slices.Equal(data, nullBody) {
+		return ErrMissingBody
+	}
+
+	err = json.Unmarshal(data, &body)
 	if err == nil {
 		return nil
 	}
@@ -26,8 +39,6 @@ func GetJSONBody(r io.Reader, body any) error {
 	)
 
 	switch {
-	case err == io.EOF:
-		return ErrMissingBody
 	case errors.As(err, &validationError):
 		return err //nolint:wrapcheck
 	case errors.As(err, &validationErrors):
