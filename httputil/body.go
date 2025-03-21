cac8b56f5302ef1198496b5e63ac8010458c8f11
Leon Clark <leon.clark@gmail.com>
2025-03-21 12:12:43 -0700

    body is already a pointer

--- httputil/body.go
+++ httputil/body.go
@@ -4,31 +4,18 @@ import (
 	"encoding/json"
 	"errors"
 	"io"
-	"slices"
 
 	"github.com/bir/iken/validation"
 )
 
 var ErrMissingBody = validation.Error{Message: "missing body"}
 
-var nullBody = []byte("null")
-
 func GetJSONBody(r io.Reader, body any) error {
 	if r == nil {
 		return ErrMissingBody
 	}
 
-	data, err := io.ReadAll(r)
-	if err != nil {
-		return validation.Error{Source: err}
-	}
-
-	// json.Unmarshal doesn't call custom unmarshalers if the body is null
-	if len(data) == 0 || slices.Equal(data, nullBody) {
-		return ErrMissingBody
-	}
-
-	err = json.Unmarshal(data, &body)
+	err := json.NewDecoder(r).Decode(body)
 	if err == nil {
 		return nil
 	}
@@ -39,6 +26,8 @@ func GetJSONBody(r io.Reader, body any) error {
 	)
 
 	switch {
+	case err == io.EOF:
+		return ErrMissingBody
 	case errors.As(err, &validationError):
 		return err //nolint:wrapcheck
 	case errors.As(err, &validationErrors):
