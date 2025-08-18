66384e70a2af86b4a466bba43f189637eb4969d6
Leon Clark <leon.clark@gmail.com>
2025-08-18 13:14:50 -0700

    set/get operaion in context: used for route operation names

--- logctx/context.go
+++ logctx/context.go
@@ -7,8 +7,9 @@ import (
 type ContextKey string
 
 const (
-	opID      ContextKey = "request_id"
-	opMessage ContextKey = "request_message"
+	operationKey ContextKey = "operation"
+	opID         ContextKey = "request_id"
+	opMessage    ContextKey = "request_message"
 )
 
 // SetID sets the request ID logged to the context.
@@ -19,6 +20,21 @@ func SetID(ctx context.Context, id string) context.Context {
 // GetID returns the request ID logged to the context, otherwise 0.
 func GetID(ctx context.Context) string {
 	s, ok := ctx.Value(opID).(string)
+	if ok {
+		return s
+	}
+
+	return ""
+}
+
+// SetOperation sets the operation logged to the context.
+func SetOperation(ctx context.Context, operation string) context.Context {
+	return context.WithValue(ctx, operationKey, operation)
+}
+
+// GetOperation returns the operation logged to the context, otherwise an empty string.
+func GetOperation(ctx context.Context) string {
+	s, ok := ctx.Value(operationKey).(string)
 	if ok {
 		return s
 	}
